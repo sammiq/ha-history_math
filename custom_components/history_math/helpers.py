273b40f86ee9b0818391d95700567d9a84ff688b
sammiq <968135+sammiq@users.noreply.github.com>
2025-01-02 02:15:23 +0000

    Remove stray function from refactor

--- custom_components/history_math/helpers.py
+++ custom_components/history_math/helpers.py
@@ -73,17 +73,6 @@ def async_calculate_period(
     return start, end
 
 
-def pretty_ratio(
-    value: float, period: tuple[datetime.datetime, datetime.datetime]
-) -> float:
-    """Format the ratio of value / period duration."""
-    if len(period) != 2 or period[0] == period[1]:
-        return 0.0
-
-    ratio = 100 * value / (period[1] - period[0]).total_seconds()
-    return round(ratio, 1)
-
-
 def floored_timestamp(incoming_dt: datetime.datetime) -> float:
     """Calculate the floored value of a timestamp."""
     return math.floor(dt_util.as_timestamp(incoming_dt))
